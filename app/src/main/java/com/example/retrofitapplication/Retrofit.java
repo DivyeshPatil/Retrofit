c5cd4cb1b0a4f1357479721420d5a85878162054
OWNER <patil.divyesh1000@gmail.com>
2021-03-11 14:35:20 +0530

    This is retrofit application

---
+++ app/src/main/java/com/example/retrofitapplication/Retrofit.java
@@ -0,0 +1,26 @@
+package com.example.retrofitapplication;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Retrofit {
+
+    static Gson gson = new GsonBuilder()
+            .setLenient()
+            .create();
+
+    private static retrofit2.Retrofit retrofit;
+    public static retrofit2.Retrofit getRetrofit()
+    {
+        if (retrofit == null) {
+            String baseUrl = "https://reqres.in/api/";
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit;
+    }
+}
